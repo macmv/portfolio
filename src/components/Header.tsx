cb3f675877cc33aaeabde8016a7b0bf456e2b4b8
Neil Macneale V <neil.macneale.v@gmail.com>
2025-04-19 16:11:24 -0700

    Make header look nicer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ export function Header() {
   return (
     <div class="header-wrapper">
       <a href="./about">About</a>
-      <a href="./about">About</a>
-      <a href="./about">About</a>
+      <a href="./skills">Skills</a>
+      <a href="./contact">Contact</a>
     </div>
   );
 }
