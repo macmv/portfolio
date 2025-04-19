f5bc3f4a8dbd1acf0eacecf14163d793198eb4fb
Neil Macneale V <neil.macneale.v@gmail.com>
2025-04-19 16:40:25 -0700

    Make links do nothing when clicked

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,7 +4,15 @@ export const Link = (props: { href: string; children: string }) => {
   // `link-content` gets an underline on hover, and `link-wrapper` has some
   // padding so the link is easier to click.
   return (
-    <a class="link-wrapper" href={props.href}>
+    <a
+      class="link-wrapper"
+      href={props.href}
+      onClick={(ev) => {
+        window.history.replaceState({}, "", props.href);
+        ev.preventDefault();
+        ev.stopPropagation();
+      }}
+    >
       <div class="link-content">{props.children}</div>
     </a>
   );
