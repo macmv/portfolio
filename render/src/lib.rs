93e0fb2ed81636ba1900f1f515f8d22012c3e844
Neil Macneale V <neil.macneale.v@gmail.com>
2025-05-14 21:40:01 -0700

    Can construct a device

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -26,11 +26,19 @@ async fn setup_instance(canvas: &wgpu::web_sys::HtmlCanvasElement) {
 
   let surface = instance.create_surface(wgpu::SurfaceTarget::Canvas(canvas.clone())).unwrap();
 
-  let _adapter = instance
+  let adapter = instance
     .request_adapter(&wgpu::RequestAdapterOptions {
       compatible_surface:     Some(&surface),
       power_preference:       wgpu::PowerPreference::default(),
       force_fallback_adapter: false,
+    })
+    .await
+    .unwrap();
+
+  let device = adapter
+    .request_device(&wgpu::DeviceDescriptor {
+      required_limits: wgpu::Limits::downlevel_webgl2_defaults(),
+      ..Default::default()
     })
     .await
     .unwrap();
