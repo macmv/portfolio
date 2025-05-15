6cfb18aded90c9637858ac51cf943811624b7e15
Neil Macneale V <neil.macneale.v@gmail.com>
2025-05-14 21:37:23 -0700

    Switch to webgl, and add console_log

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -11,6 +11,7 @@ extern "C" {
 #[wasm_bindgen]
 pub async fn setup_render(canvas: &wgpu::web_sys::HtmlCanvasElement) {
   std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+  let _ = console_log::init_with_level(log::Level::Trace);
 
   setup_instance(canvas).await;
 
@@ -19,15 +20,15 @@ pub async fn setup_render(canvas: &wgpu::web_sys::HtmlCanvasElement) {
 
 async fn setup_instance(canvas: &wgpu::web_sys::HtmlCanvasElement) {
   let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-    backends: wgpu::Backends::BROWSER_WEBGPU,
+    backends: wgpu::Backends::GL,
     ..Default::default()
   });
 
-  let _surface = instance.create_surface(wgpu::SurfaceTarget::Canvas(canvas.clone()));
+  let surface = instance.create_surface(wgpu::SurfaceTarget::Canvas(canvas.clone())).unwrap();
 
   let _adapter = instance
     .request_adapter(&wgpu::RequestAdapterOptions {
-      compatible_surface:     None,
+      compatible_surface:     Some(&surface),
       power_preference:       wgpu::PowerPreference::default(),
       force_fallback_adapter: false,
     })
