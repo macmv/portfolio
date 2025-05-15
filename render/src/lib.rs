482db398919c33ebd0e4b9efd6d7f1045a3e6996
Neil Macneale V <neil.macneale.v@gmail.com>
2025-05-14 21:09:47 -0700

    Make things async, and add console_error_panic_hook

--- render/src/lib.rs
+++ render/src/lib.rs
@@ -9,13 +9,15 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn setup_render(canvas: &wgpu::web_sys::HtmlCanvasElement) {
-  setup_instance(canvas);
+pub async fn setup_render(canvas: &wgpu::web_sys::HtmlCanvasElement) {
+  std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+  setup_instance(canvas).await;
 
   alert(&format!("Hello, world!"));
 }
 
-fn setup_instance(canvas: &wgpu::web_sys::HtmlCanvasElement) {
+async fn setup_instance(canvas: &wgpu::web_sys::HtmlCanvasElement) {
   let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
     backends: wgpu::Backends::BROWSER_WEBGPU,
     ..Default::default()
@@ -23,10 +25,12 @@ fn setup_instance(canvas: &wgpu::web_sys::HtmlCanvasElement) {
 
   let _surface = instance.create_surface(wgpu::SurfaceTarget::Canvas(canvas.clone()));
 
-  let _adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
-    compatible_surface:     None,
-    power_preference:       wgpu::PowerPreference::default(),
-    force_fallback_adapter: false,
-  }))
-  .unwrap();
+  let _adapter = instance
+    .request_adapter(&wgpu::RequestAdapterOptions {
+      compatible_surface:     None,
+      power_preference:       wgpu::PowerPreference::default(),
+      force_fallback_adapter: false,
+    })
+    .await
+    .unwrap();
 }
