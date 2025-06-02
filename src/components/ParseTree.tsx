4c02a94ffa206be3472785c88454cc25f8e4a19f
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-02 14:53:18 -0700

    Add a bit of styling to make an actual tree shape

--- src/components/ParseTree.tsx
+++ src/components/ParseTree.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "preact/hooks";
 import init, { parse } from "../../render/pkg";
 
+import "./ParseTree.css";
+
 type Tree = Array<Node>;
 type Node =
   | { type: "binary"; left: number; right: number; operator: string }
@@ -18,7 +20,7 @@ export const ParseTree = (props: { code: string }) => {
   if (tree === null) {
     return <></>;
   } else if (typeof tree === "string") {
-    return <div class="parse-tree">{tree}</div>;
+    return <div class="parse-error">{tree}</div>;
   } else {
     const renderNode = (idx: number) => {
       const node = tree[idx];
@@ -29,16 +31,18 @@ export const ParseTree = (props: { code: string }) => {
 
         return (
           <span class="binary">
-            {left} {node.operator} {right}
+            <span class="child">{left}</span>
+            <span class="node">{node.operator}</span>
+            <span class="child">{right}</span>
           </span>
         );
       } else if (node.type === "literal") {
-        return <span class="binary">{node.value}</span>;
+        return <span class="node">{node.value}</span>;
       } else {
         return <></>;
       }
     };
 
-    return renderNode(tree.length - 1);
+    return <div class="parse-tree">{renderNode(tree.length - 1)}</div>;
   }
 };
