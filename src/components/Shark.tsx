77b128d4c8be974b576dee6091acba4d50612764
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-03 01:55:13 -0700

    The sharks move along the bottom of the screen

--- src/components/Shark.tsx
+++ src/components/Shark.tsx
@@ -1,17 +1,50 @@
-import { useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import "./Shark.css";
 
 export const Shark = () => {
   const [sharks, setSharks] = useState<number[]>([]);
 
+  const animation = useRef<number | null>(null);
+  const speed = 3.0; // pixels per frame
+
+  const frame = () => {
+    setSharks((s) => {
+      return s.map((x) => x + speed).filter((x) => x < window.innerWidth + 100);
+    });
+
+    if (sharks.length > 0) {
+      animation.current = requestAnimationFrame(frame);
+    }
+  };
+
+  useEffect(() => {
+    if (animation.current === null) {
+      animation.current = requestAnimationFrame(frame);
+    }
+
+    return () => {
+      if (animation.current !== null) {
+        cancelAnimationFrame(animation.current);
+        animation.current = null;
+      }
+    };
+  }, [sharks.length, animation.current]);
+
   return (
-    <span
-      class="shark-button"
-      onClick={() => {
-        setSharks((s) => [0, ...s]);
-      }}
-    >
-      Shark!
-    </span>
+    <>
+      <span
+        class="shark-button"
+        onClick={() => {
+          setSharks((s) => [0, ...s]);
+        }}
+      >
+        Shark!
+      </span>
+      {sharks.map((x, idx) => (
+        <span class="shark" style={{ marginLeft: x, bottom: 0 }}>
+          Shark {idx + 1}
+        </span>
+      ))}
+    </>
   );
 };
