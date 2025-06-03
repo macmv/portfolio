fcecc2c3977d813f9d855dfaaf7f9bb9573ff9b2
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-03 15:45:00 -0700

    Fix the highlight spacing, and make borders more concrete

--- src/app.tsx
+++ src/app.tsx
@@ -64,27 +64,35 @@ const Parsers = () => {
             alignItems: "center",
           }}
         >
-          {highlight && (
-            <span
-              class="highlight"
-              dangerouslySetInnerHTML={{
-                __html:
-                  "&nbsp;".repeat(highlight[0]) +
-                  "_".repeat(highlight[1] - highlight[0]),
-              }}
+          <div>
+            {highlight && (
+              <div class="highlight">
+                <span
+                  class="spacer"
+                  dangerouslySetInnerHTML={{
+                    __html: "&nbsp;".repeat(highlight[0]),
+                  }}
+                />
+                <span
+                  class="underline"
+                  dangerouslySetInnerHTML={{
+                    __html: "&nbsp;".repeat(highlight[1] - highlight[0]),
+                  }}
+                />
+              </div>
+            )}
+            <input
+              type="text"
+              class="code-input"
+              data-gramm="false"
+              data-gramm_editor="false"
+              data-enable-grammarly="false"
+              data-lt-active="false"
+              value={code}
+              onKeyUp={(e) => setCode((e.target as HTMLTextAreaElement).value)}
+              onChange={(e) => setCode((e.target as HTMLTextAreaElement).value)}
             />
-          )}
-          <input
-            type="text"
-            class="code-input"
-            data-gramm="false"
-            data-gramm_editor="false"
-            data-enable-grammarly="false"
-            data-lt-active="false"
-            value={code}
-            onKeyUp={(e) => setCode((e.target as HTMLTextAreaElement).value)}
-            onChange={(e) => setCode((e.target as HTMLTextAreaElement).value)}
-          />
+          </div>
           <p style={{ maxWidth: "400px" }}>
             This is a simple recursive descent parser, that builds a parse tree
             for a mathmatical expression. Each operation is represented as a
