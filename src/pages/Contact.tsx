0c76c9be08861df575f35894ca9c410f1d92c7fe
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-04 01:37:00 -0700

    Add some styling to the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,21 @@
 import { Banner } from "../components/Banner";
 import { Physics } from "../components/Physics";
+import "./Contact.css";
 
 export const Contact = () => {
   return (
     <>
       <Banner title="Contact" />
-      <div>Contact me!</div>
 
-      <span class="skills-intro">And, for fun:</span>
+      <div class="contact-outer">
+        <div class="contact-wrapper">
+          <div>Contact me!</div>
 
-      <Physics />
+          <span class="intro">And, for fun:</span>
+
+          <Physics />
+        </div>
+      </div>
     </>
   );
 };
