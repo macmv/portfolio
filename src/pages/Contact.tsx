cbc33b3bad28619a28a530a0d76a6f9b34db2d74
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-04 01:33:55 -0700

    Move the physics to the contact me page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,15 @@
 import { Banner } from "../components/Banner";
+import { SkillsSimulation } from "../components/SkillsSimulation";
 
 export const Contact = () => {
   return (
     <>
       <Banner title="Contact" />
       <div>Contact me!</div>
+
+      <span class="skills-intro">And, for fun:</span>
+
+      <SkillsSimulation />
     </>
   );
 };
