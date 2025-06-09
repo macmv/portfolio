aaf2e1f488b14b94f30d93e1dc22d394003c06ec
Neil Macneale V <neil.macneale.v@gmail.com>
2025-06-09 15:49:31 -0700

    Add a list

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -10,23 +10,33 @@ export const Contact = () => {
       <div class="contact-outer">
         <div class="contact-wrapper">
           <span>
-            I'm reachable by email at:{" "}
-            <a href="mailto:neil.macneale.v@gmail.com" target="_blank">
-              neil.macneale.v@gmail.com
-            </a>
-            .
-            <br />
-            My Github is:{" "}
-            <a href="https://github.com/macmv" target="_blank">
-              https://github.com/macmv
-            </a>
-            .
-            <br />
-            And my LinkedIn is:{" "}
-            <a href="https://www.linkedin.com/in/neilmacnealev" target="_blank">
-              https://www.linkedin.com/in/neilmacnealev
-            </a>
-            .
+            I'm reachable via:
+            <ul>
+              <li>
+                Email:{" "}
+                <a href="mailto:neil.macneale.v@gmail.com" target="_blank">
+                  neil.macneale.v@gmail.com
+                </a>
+                .
+              </li>
+              <li>
+                On Github:{" "}
+                <a href="https://github.com/macmv" target="_blank">
+                  https://github.com/macmv
+                </a>
+                .
+              </li>
+              <li>
+                On LinkedIn:{" "}
+                <a
+                  href="https://www.linkedin.com/in/neilmacnealev"
+                  target="_blank"
+                >
+                  https://www.linkedin.com/in/neilmacnealev
+                </a>
+                .
+              </li>
+            </ul>
           </span>
 
           <span class="intro">And, for fun:</span>
