668e7561b0dd05288b6165700ceafddfebe5e2da
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-20 10:43:24 -0800

    Setup a sveltekit project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), wasm()],
-  build: {
-    target: "esnext",
-  },
+  plugins: [sveltekit(), wasm()],
 });
