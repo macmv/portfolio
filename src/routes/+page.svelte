d85256f275a113064343d7c203c2fdc6fb2df6c3
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-20 19:29:00 -0800

    Add the link component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+  import Link from "$lib/link.svelte";
+</script>
+
 <main>
-  <h1>Home</h1>
+  <div class="flex flex-row gap-4 justify-center">
+    <Link href="/">Home</Link>
+    <Link href="/parsers">Parsers</Link>
+    <Link href="/skills">Skills</Link>
+    <Link href="/sharks">Sharks!</Link>
+    <Link href="/contact">Contact</Link>
+  </div>
   <p>Home page</p>
 </main>
