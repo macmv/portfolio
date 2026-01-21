da2899f66e05140e9aabba36bb455a0aac1ea407
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-20 19:35:14 -0800

    Put the links in the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import Link from "$lib/link.svelte";
   import "../app.css";
 
   const { children } = $props();
 </script>
 
-{@render children()}
+<main>
+  <div class="flex flex-row gap-4 justify-center">
+    <Link href="/">Home</Link>
+    <Link href="/parsers">Parsers</Link>
+    <Link href="/skills">Skills</Link>
+    <Link href="/sharks">Sharks!</Link>
+    <Link href="/contact">Contact</Link>
+  </div>
+  {@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,1 @@
-<script>
-  import Link from "$lib/link.svelte";
-</script>
-
-<main>
-  <div class="flex flex-row gap-4 justify-center">
-    <Link href="/">Home</Link>
-    <Link href="/parsers">Parsers</Link>
-    <Link href="/skills">Skills</Link>
-    <Link href="/sharks">Sharks!</Link>
-    <Link href="/contact">Contact</Link>
-  </div>
-  <p>Home page</p>
-</main>
+<p>Home page</p>
