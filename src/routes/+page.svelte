6e96fbccad116389a05e0496f46854f80c1b37f8
Neil Macneale V <neil.macneale.v@gmail.com>
2026-01-21 03:08:48 -0800

    Add the home page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-<p>Home page</p>
+<main class="h-screen grid place-content-center text-center gap-8">
+  <h1>Neil Macneale</h1>
+  <h2>Full Stack Developer and Student</h2>
+</main>
+
+<style>
+  h1,
+  h2 {
+    background: #0000;
+    font-weight: bold;
+
+    color: var(--text);
+    text-shadow: 0 10px 20px #fff;
+  }
+
+  h2 {
+    font-size: min(40px, 3.5vw);
+  }
+
+  h1 {
+    font-size: min(120px, 10vw);
+  }
+</style>
